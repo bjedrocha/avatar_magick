00cb0bfe80f484873a963d73feb7a9e8aaf6f67d
Jordi <Jordi@amalgama.co>
2019-09-30 18:20:55 -0300

    Add font size as a param for the generator

--- lib/avatar_magick/plugin.rb
+++ lib/avatar_magick/plugin.rb
@@ -12,6 +12,7 @@ module AvatarMagick
       app.env[:avatar_magick][:background_color]  = opts[:background_color] ? "##{opts[:background_color]}" : "#000000"
       app.env[:avatar_magick][:size]              = opts[:size] || "120x120"
       app.env[:avatar_magick][:font]              = opts[:font] || "Arial-Regular"
+      app.env[:avatar_magick][:font_size]         = opts[:font_size]
 
       app.add_generator :initial_avatar, AvatarMagick::Generators::InitialAvatar.new
     end
